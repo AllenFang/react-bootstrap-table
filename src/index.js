10783ef7ef4aaab50090c693867a0b6964f0acea
AllenFang <ayu780129@hotmail.com>
2015-04-11 16:37:11 +0800

    fix TableHeaderColumn doesn't export to window object.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import BootstrapTable from './BootstrapTable';
 import TableHeaderColumn from './TableHeaderColumn'
 window.BootstrapTable = BootstrapTable;
+window.TableHeaderColumn = TableHeaderColumn;
 export default {
   BootstrapTable,
   TableHeaderColumn
