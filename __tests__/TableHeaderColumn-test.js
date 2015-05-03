68b647714bea8505c4ddc9efc2e3d1be0417747a
AllenFang <ayu780129@hotmail.com>
2015-05-03 14:42:57 +0800

    update table header testing.

--- __tests__/TableHeaderColumn-test.js
+++ __tests__/TableHeaderColumn-test.js
@@ -18,13 +18,12 @@ describe('TableHeaderColumn Test', function() {
     React = require('react/addons');
     TableHeaderColumn = require('../src/TableHeaderColumn.js');
     TestUtils = React.addons.TestUtils;
-    ReactTestUtils = React.addons.ReactTestUtils;
   });
 
   it('A basic table header column test', function() {
 
     var idHeaderColumn = TestUtils.renderIntoDocument(
-      <TableHeaderColumn dataField="id">Product ID</TableHeaderColumn>
+      <TableHeaderColumn dataField="id" isKey={true}>Product ID</TableHeaderColumn>
     );
 
     expect(idHeaderColumn.props.dataField).toEqual("id");
@@ -32,6 +31,7 @@ describe('TableHeaderColumn Test', function() {
     expect(idHeaderColumn.props.dataAlign).toEqual("left");
     expect(idHeaderColumn.props.dataSort).toEqual(false);
     expect(idHeaderColumn.props.dataFormat).toBe(undefined);
+    expect(idHeaderColumn.props.isKey).toBe(true);
 
 
     var reactThElm = TestUtils.findRenderedDOMComponentWithTag(idHeaderColumn, 'th');
@@ -39,6 +39,7 @@ describe('TableHeaderColumn Test', function() {
     var th = reactThElm.getDOMNode();
     expect(th.className).toEqual("");
     expect(th.childNodes.length).toEqual(1);
+    expect(th.style.textAlign).toEqual("left");
 
     var reactDivElm = TestUtils.findRenderedDOMComponentWithTag(idHeaderColumn, 'div');
     expect(reactDivElm).not.toBe(null);
@@ -48,5 +49,83 @@ describe('TableHeaderColumn Test', function() {
 
     // ReactTestUtils.Simulate.click(reactDivElm);
     // expect(th.getDOMNode()).toEqual(0);
+  });
+
+  it('A align header column test', function(){
+    var idHeaderColumn = TestUtils.renderIntoDocument(
+      <TableHeaderColumn dataField="id" dataAlign="center">Product ID</TableHeaderColumn>
+    );
+
+    expect(idHeaderColumn.props.dataField).toEqual("id");
+    expect(idHeaderColumn.props.children).toEqual("Product ID");
+    expect(idHeaderColumn.props.dataAlign).toEqual("center");
+    expect(idHeaderColumn.props.dataSort).toEqual(false);
+    expect(idHeaderColumn.props.dataFormat).toBe(undefined);
+    expect(idHeaderColumn.props.isKey).toBe(false);
+
+    var reactThElm = TestUtils.findRenderedDOMComponentWithTag(idHeaderColumn, 'th');
+    expect(reactThElm).not.toBe(null);
+    expect(reactThElm.getDOMNode().style.textAlign).toEqual("center");
+  });
+
+  it('A unique header column test', function(){
+    var idHeaderColumn = TestUtils.renderIntoDocument(
+      <TableHeaderColumn dataField="id" isKey={true}>Product ID</TableHeaderColumn>
+    );
+
+    expect(idHeaderColumn.props.dataField).toEqual("id");
+    expect(idHeaderColumn.props.children).toEqual("Product ID");
+    expect(idHeaderColumn.props.dataAlign).toEqual("left");
+    expect(idHeaderColumn.props.dataSort).toEqual(false);
+    expect(idHeaderColumn.props.dataFormat).toBe(undefined);
+    expect(idHeaderColumn.props.isKey).toBe(true);
+  });
+
+  it('A dataformat header column test', function(){
+    function priceFormatter(cell, row){
+      return '<i class="glyphicon glyphicon-usd"></i> ' + cell;
+    }
+
+    var idHeaderColumn = TestUtils.renderIntoDocument(
+      <TableHeaderColumn dataField="id" dataFormat={priceFormatter}>Product ID</TableHeaderColumn>
+    );
+
+    expect(idHeaderColumn.props.dataField).toEqual("id");
+    expect(idHeaderColumn.props.children).toEqual("Product ID");
+    expect(idHeaderColumn.props.dataAlign).toEqual("left");
+    expect(idHeaderColumn.props.dataSort).toEqual(false);
+    expect(idHeaderColumn.props.dataFormat).toEqual(priceFormatter);
+    expect(idHeaderColumn.props.isKey).toBe(false);
+  });
+
+  it('A sort header column test', function(){
+    var idHeaderColumn = TestUtils.renderIntoDocument(
+      <TableHeaderColumn dataField="id" dataSort={true}>Product ID</TableHeaderColumn>
+    );
+
+    expect(idHeaderColumn.props.dataField).toEqual("id");
+    expect(idHeaderColumn.props.children).toEqual("Product ID");
+    expect(idHeaderColumn.props.dataAlign).toEqual("left");
+    expect(idHeaderColumn.props.dataSort).toEqual(true);
+    expect(idHeaderColumn.props.dataFormat).toBe(undefined);
+    expect(idHeaderColumn.props.isKey).toBe(false);
+
+    var reactThElm = TestUtils.findRenderedDOMComponentWithTag(idHeaderColumn, 'th');
+    expect(reactThElm).not.toBe(null);
+    var th = reactThElm.getDOMNode();
+    console.log(th.className);
+    expect(th.className).toEqual("sort-column");
+    expect(th.childNodes.length).toEqual(1);
+    expect(th.style.textAlign).toEqual("left");
+
+    var reactDivElm = TestUtils.findRenderedDOMComponentWithTag(idHeaderColumn, 'div');
+    expect(reactDivElm).not.toBe(null);
+    var div = reactDivElm.getDOMNode();
+    expect(div.className).toEqual("th-inner table-header-column");
+    expect(div.textContent).toEqual("Product ID");
+
+    TestUtils.Simulate.click(reactDivElm);
+    expect(div.childNodes.length).toEqual(2);
+    console.log(div.childNodes[1]);
   });
 });
