9dfe7d5986f8601e7eeb3772f3c01464a81e1f37
AllenFang <ayu780129@hotmail.com>
2015-08-27 20:36:22 +0800

    fix issues#27 temporary.

--- src/BootstrapTable.js
+++ src/BootstrapTable.js
@@ -258,8 +258,10 @@ class BootstrapTable extends React.Component{
   }
 
   _adjustHeaderWidth(){
-    this.refs.table.getDOMNode().childNodes[0].childNodes[0].style.width =
-      this.refs.table.getDOMNode().childNodes[1].childNodes[0].offsetWidth-1+"px";
+    if(this.refs.table){
+      this.refs.table.getDOMNode().childNodes[0].childNodes[0].style.width =
+        this.refs.table.getDOMNode().childNodes[1].childNodes[0].offsetWidth-1+"px";
+    }
   }
 
   renderPagination(){
