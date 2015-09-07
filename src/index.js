8a50fffcfb280a275bb63ff3a54d15cf962412fb
AllenFang <ayu780129@hotmail.com>
2015-09-07 23:32:18 +0800

    fix #45

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import BootstrapTable from './BootstrapTable';
 import TableHeaderColumn from './TableHeaderColumn';
 import {TableDataSet} from './store/TableDataStore';
+
+if(window){
+  window.BootstrapTable = BootstrapTable;
+  window.TableHeaderColumn = TableHeaderColumn;
+}
 export default {
   BootstrapTable,
   TableHeaderColumn,
