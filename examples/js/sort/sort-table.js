64c24eb393bffe31e7bed3e16bb69566963b81b3
AllenFang <ayu780129@hotmail.com>
2016-01-15 23:34:05 +0800

    example for #212

--- examples/js/sort/sort-table.js
+++ examples/js/sort/sort-table.js
@@ -18,15 +18,30 @@ function addProducts(quantity) {
 }
 
 addProducts(5);
-
+var order = 'desc';
 export default class SortTable extends React.Component{
+
+  handleBtnClick = e => {
+    if(order === 'desc'){
+      this.refs.table.handleSort('asc', 'name');
+      order = 'asc';
+    } else {
+      this.refs.table.handleSort('desc', 'name');
+      order = 'desc';
+    }
+  }
+
   render(){
     return (
-      <BootstrapTable data={products}>
-          <TableHeaderColumn dataField="id" isKey={true} dataSort={true}>Product ID</TableHeaderColumn>
-          <TableHeaderColumn dataField="name" dataSort={true}>Product Name</TableHeaderColumn>
-          <TableHeaderColumn dataField="price">Product Price</TableHeaderColumn>
-      </BootstrapTable>
+      <div>
+        <p style={{color:'red'}}>You cam click header to sort or click following button to perform a sorting by expose API</p>
+        <button onClick={this.handleBtnClick}>Sort Product Name</button>
+        <BootstrapTable ref="table" data={products}>
+            <TableHeaderColumn dataField="id" isKey={true} dataSort={true}>Product ID</TableHeaderColumn>
+            <TableHeaderColumn dataField="name" dataSort={true}>Product Name</TableHeaderColumn>
+            <TableHeaderColumn dataField="price">Product Price</TableHeaderColumn>
+        </BootstrapTable>
+      </div>
     );
   }
 };
