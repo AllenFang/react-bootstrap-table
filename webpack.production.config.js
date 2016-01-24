9723c1fe4e3f8c69dad1378472a3ae43db195d4d
Fatih <fatih@picussecurity.com>
2016-01-24 11:10:03 +0200

    make react-onclickoutside a runtime dependency

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -21,6 +21,14 @@ module.exports = {
       }
     },
     {
+      'react-onclickoutside': {
+        root: 'OnClickOutside',
+        commonjs2: 'react-onclickoutside',
+        commonjs: 'react-onclickoutside',
+        amd: 'react-onclickoutside'
+      }
+    },
+    {
       'react-dom': {
         root: 'ReactDOM',
         commonjs2: 'react-dom',
