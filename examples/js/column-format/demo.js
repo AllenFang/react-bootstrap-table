9400faf765362023e4f878ac693f3c69cf103594
AllenFang <ayu780129@hotmail.com>
2016-01-30 13:47:21 +0800

    add column format with extra data example

--- examples/js/column-format/demo.js
+++ examples/js/column-format/demo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HtmlColumnFormatTable from './html-column-format-table';
 import ReactColumnFormatTable from './react-column-format-table';
+import FormatExtraDataTable from './format-extra-data-table';
 
 class Demo extends React.Component {
 	render() {
@@ -12,6 +13,15 @@ class Demo extends React.Component {
 							<div className="panel-body">
 								<h5>Source in /examples/js/column-format/html-column-format-table.js</h5>
 								<HtmlColumnFormatTable />
+							</div>
+						</div>
+					</div>
+					<div className="col-md-offset-1 col-md-8">
+						<div className="panel panel-default">
+							<div className="panel-heading">Column Format with extra data</div>
+							<div className="panel-body">
+								<h5>Source in /examples/js/column-format/format-extra-data-table.js</h5>
+								<FormatExtraDataTable />
 							</div>
 						</div>
 					</div>
