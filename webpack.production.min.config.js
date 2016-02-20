3826725c8dd6d73ed705fb0e96175519867c3aa2
AllenFang <ayu780129@hotmail.com>
2016-02-20 16:05:35 +0800

    add webpack base config

--- webpack.production.min.config.js
+++ webpack.production.min.config.js
@@ -1,42 +1,17 @@
 var webpack = require('webpack');
+var base = require('./webpack.base.config');
 
 module.exports = {
   devtool: '#eval-source-map',
-  entry: {
-    'react-bootstrap-table': './src/index.js'
-  },
+  entry: base.entry,
   output: {
     path: './dist',
     filename: '[name].min.js',
     library: 'ReactBootstrapTable',
     libraryTarget: 'umd'
   },
-  externals: [
-    {
-      'react': {
-        root: 'React',
-        commonjs2: 'react',
-        commonjs: 'react',
-        amd: 'react'
-      }
-    },
-    {
-      'react-dom': {
-        root: 'ReactDOM',
-        commonjs2: 'react-dom',
-        commonjs: 'react-dom',
-        amd: 'react-dom'
-      }
-    }
-  ],
-  module: {
-    loaders: [{
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: ['babel']
-      }]
-  },
-
+  externals: base.externals,
+  module: base.module,
   plugins: [
     new webpack.optimize.UglifyJsPlugin()
   ]
