c3a5ef82f281ea94072470003b816a5ad8c06f51
AllenFang <ayu780129@hotmail.com>
2016-03-13 14:41:13 +0800

    add table in tab example

--- examples/js/others/demo.js
+++ examples/js/others/demo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import MouseEventTable from './mouse-event-table';
-
+import TableInTabs from './table-in-tabs';
 import {Col, Panel} from 'react-bootstrap';
 
 class Demo extends React.Component {
@@ -11,6 +11,11 @@ class Demo extends React.Component {
           <h5>Source in /examples/js/advance/mouse-event-table.js</h5>
           <h5>See event ouput in browser console</h5>
           <MouseEventTable/>
+        </Panel>
+        <Panel header={"Table in Tabs Example)"}>
+          <h5>Source in /examples/js/advance/table-in-tabs.js</h5>
+          <h5>react-bootstrap-table in tabs</h5>
+          <TableInTabs/>
         </Panel>
       </Col>
     );
---
+++ examples/js/others/table-in-tabs.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Tabs, Tab} from 'react-bootstrap';
+
+var products = [];
+
+function addProducts(quantity) {
+  var startId = products.length;
+  for (var i = 0; i < quantity; i++) {
+    var id = startId + i;
+    products.push({
+      id: id,
+      name: "Item name " + id,
+      price: 2100 + i
+    });
+  }
+}
+
+addProducts(5);
+
+export default class TableInTabs extends React.Component {
+    render() {
+      return(
+          <Tabs defaultActiveKey={2}>
+              <Tab eventKey={1} title="Tab 1">
+                  <BootstrapTable data={products}>
+                      <TableHeaderColumn dataField="id" isKey={true} dataSort={true}>Product ID</TableHeaderColumn>
+                      <TableHeaderColumn dataField="name" dataSort={true}>Product Name</TableHeaderColumn>
+                      <TableHeaderColumn dataField="price">Product Price</TableHeaderColumn>
+                  </BootstrapTable>
+              </Tab>
+              <Tab eventKey={2} title="Tab 2">Tab 3 content</Tab>
+              <Tab eventKey={3} title="Tab 3">Tab 3 content</Tab>
+          </Tabs>
+      );
+    }
+}
