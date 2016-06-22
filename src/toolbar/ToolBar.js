ff04929e7731416d10f625a5bf8751cd8b1e849a
Michael Scharf <github@scharf.gr>
2016-06-22 16:35:39 +0200

    Use `btn-success` for the Save button
    see http://getbootstrap.com/css/#buttons-options
    
    The save button is not a `informational alert message`... This can confuse users.
    
    ```html
    <!-- Indicates a successful or positive action -->
    <button type="button" class="btn btn-success">Success</button>
    
    <!-- Contextual button for informational alert messages -->
    <button type="button" class="btn btn-info">Info</button>
    ```

--- src/toolbar/ToolBar.js
+++ src/toolbar/ToolBar.js
@@ -351,7 +351,7 @@ class ToolBar extends Component {
                 { this.props.closeText }
               </button>
               <button type='button'
-                className='btn btn-info'
+                className='btn btn-success'
                 onClick={ this.handleSaveBtnClick }>
                 { this.props.saveText }
               </button>
