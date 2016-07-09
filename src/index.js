c9b979e9f501f3f4ecc8effc2c97f789f4cb3d45
AllenFang <ayu780129@hotmail.com>
2016-07-09 20:35:38 +0800

    export insert modal component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import BootstrapTable from './BootstrapTable';
 import TableHeaderColumn from './TableHeaderColumn';
+import InsertModalHeader from './toolbar/InsertModalHeader';
+import InsertModalBody from './toolbar/InsertModalBody';
+import InsertModalFooter from './toolbar/InsertModalFooter';
 
 if (typeof window !== 'undefined') {
   window.BootstrapTable = BootstrapTable;
   window.TableHeaderColumn = TableHeaderColumn;
+  window.InsertModalHeader = InsertModalHeader;
+  window.InsertModalBody = InsertModalBody;
+  window.InsertModalFooter = InsertModalFooter;
 }
 export {
   BootstrapTable,
-  TableHeaderColumn
+  TableHeaderColumn,
+  InsertModalHeader,
+  InsertModalBody,
+  InsertModalFooter
 };
