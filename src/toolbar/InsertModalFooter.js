44b408e139e2034ecfd10a10d59740b5152a58da
AllenFang <ayu780129@hotmail.com>
2016-07-09 14:02:25 +0800

    add btn contextual and className

--- src/toolbar/InsertModalFooter.js
+++ src/toolbar/InsertModalFooter.js
@@ -1,26 +1,43 @@
 import React, { PropTypes } from 'react';
 import Const from '../Const';
-const InsertModalFooter = ({ onModalClose, onSave }) =>
+const InsertModalFooter = ({
+  saveBtnText,
+  closeBtnText,
+  closeBtnContextual,
+  saveBtnContextual,
+  closeBtnClass,
+  saveBtnClass,
+  onModalClose,
+  onSave
+}) =>
   <div>
     <button
       type='button'
-      className='btn btn-default'
-      onClick={ onModalClose }>Close</button>
+      className={ `btn ${closeBtnContextual} ${closeBtnClass}` }
+      onClick={ onModalClose }>{ closeBtnText }</button>
     <button
       type='button'
-      className='btn btn-primary'
-      onClick={ onSave }>Save</button>
+      className={ `btn ${saveBtnContextual} ${saveBtnClass}` }
+      onClick={ onSave }>{ saveBtnText }</button>
   </div>;
 
 InsertModalFooter.propTypes = {
   saveBtnText: PropTypes.string,
   closeBtnText: PropTypes.string,
+  closeBtnContextual: PropTypes.string,
+  saveBtnContextual: PropTypes.string,
+  closeBtnClass: PropTypes.string,
+  saveBtnClass: PropTypes.string,
   onSave: PropTypes.func,
   onModalClose: PropTypes.func
 };
 InsertModalFooter.defaultProps = {
   saveBtnText: Const.SAVE_BTN_TEXT,
   closeBtnText: Const.CLOSE_BTN_TEXT,
+  closeBtnContextual: 'btn-default',
+  saveBtnContextual: 'btn-primary',
+  closeBtnClass: '',
+  saveBtnClass: '',
   onSave: PropTypes.func,
   onModalClose: PropTypes.func
 };
