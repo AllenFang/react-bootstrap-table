b086e793352cfdd52aec2eaeb6b3800635f7cd06
Fernando Barrios <fer.b.1990@gmail.com>
2016-08-11 17:24:08 -0600

    add support for custom multi select component in table Header

--- src/TableHeader.js
+++ src/TableHeader.js
@@ -53,7 +53,15 @@ class TableHeader extends Component {
   }
 
   renderSelectRowHeader() {
-    if (this.props.rowSelectType === Const.ROW_SELECT_SINGLE) {
+    if (this.props.customComponent) {
+      const CustomComponent = this.props.customComponent;
+      const checkedStatus = this.props.isSelectAll !== 'indeterminate' ?
+        this.props.isSelectAll : '';
+      const indeterminate = this.props.isSelectAll === 'indeterminate' ? 'indeterminate' : '';
+      return (<CustomComponent type='checkbox' checked={ checkedStatus }
+        indeterminate={ indeterminate } disabled={ false }
+        onChange={ this.props.onSelectAllRow } rowIndex='Header'/>);
+    } else if (this.props.rowSelectType === Const.ROW_SELECT_SINGLE) {
       return (<SelectRowHeaderColumn />);
     } else if (this.props.rowSelectType === Const.ROW_SELECT_MULTI) {
       return (
@@ -81,7 +89,8 @@ TableHeader.propTypes = {
   condensed: PropTypes.bool,
   isFiltered: PropTypes.bool,
   isSelectAll: PropTypes.oneOf([ true, 'indeterminate', false ]),
-  sortIndicator: PropTypes.bool
+  sortIndicator: PropTypes.bool,
+  customComponent: PropTypes.func
 };
 
 export default TableHeader;
