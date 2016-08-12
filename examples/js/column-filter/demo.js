7d6ded2d2467f8d3b5dbc303c7bc05b1977d471a
AllenFang <ayu780129@hotmail.com>
2016-08-12 23:17:23 +0800

    example for #548

--- examples/js/column-filter/demo.js
+++ examples/js/column-filter/demo.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import TextFilter from './text-filter';
 import TextFilterWithDefaultValue from './text-filter-with-default-value';
+import ProgrammaticallyTextFilter from './text-filter-programmatically';
 import RegexFilter from './regex-filter';
 import SelectFilter from './select-filter';
 import SelectFilterWithDefaultValue from './select-filter-with-default-value';
@@ -35,6 +36,15 @@ class Demo extends React.Component {
                     <div className='panel-body'>
                         <h5>Source in /examples/js/column-filter/text-filter-with-default-value.js</h5>
                         <TextFilterWithDefaultValue />
+                    </div>
+                </div>
+            </div>
+            <div className='col-md-offset-1 col-md-8'>
+                <div className='panel panel-default'>
+                    <div className='panel-heading'>Set Text Filter Programmatically</div>
+                    <div className='panel-body'>
+                        <h5>Source in /examples/js/column-filter/text-filter-programmatically.js</h5>
+                        <ProgrammaticallyTextFilter />
                     </div>
                 </div>
             </div>
---
+++ examples/js/column-filter/text-filter-programmatically.js
@@ -0,0 +1,39 @@
+/* eslint max-len: 0 */
+import React from 'react';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+
+const products = [];
+
+function addProducts(quantity) {
+  const startId = products.length;
+  for (let i = 0; i < quantity; i++) {
+    const id = startId + i;
+    products.push({
+      id: id,
+      name: 'Item name ' + id,
+      price: 2100 + i
+    });
+  }
+}
+
+addProducts(5);
+
+export default class ProgrammaticallyTextFilter extends React.Component {
+
+  handleBtnClick = () => {
+    this.refs.nameCol.applyFilter('Item name 3');
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={ this.handleBtnClick } className='btn btn-default'>Click to apply text filter</button>
+        <BootstrapTable data={ products }>
+          <TableHeaderColumn dataField='id' isKey={ true }>Product ID</TableHeaderColumn>
+          <TableHeaderColumn ref='nameCol' dataField='name' filter={ { type: 'TextFilter', delay: 1000 } }>Product Name</TableHeaderColumn>
+          <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
+        </BootstrapTable>
+      </div>
+    );
+  }
+}
