c6d2053836619e8cff6ac6e1cb22fe50e32f88aa
AllenFang <ayu780129@hotmail.com>
2016-08-27 15:03:30 +0800

    example for containerClass, tableContainerClass, headerContainerClass, bodyContainerClass

--- examples/js/style/table-class-table.js
+++ examples/js/style/table-class-table.js
@@ -23,7 +23,13 @@ addProducts(5);
 export default class TableClassTable extends React.Component {
   render() {
     return (
-      <BootstrapTable data={ products } tableHeaderClass='my-header-class' tableBodyClass='my-body-class'>
+      <BootstrapTable data={ products } insertRow exportCSV
+          tableHeaderClass='my-header-class'
+          tableBodyClass='my-body-class'
+          containerClass='my-container-class'
+          tableContainerClass='my-table-container-class'
+          headerContainerClass='my-header-container-class'
+          bodyContainerClass='my-body-container-class'>
           <TableHeaderColumn dataField='id' isKey={ true }>Product ID</TableHeaderColumn>
           <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
           <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
