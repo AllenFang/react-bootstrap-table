0772e99e52e0952248b8ccd0e0ac07123bcf0206
sumbad <sumbad@ya.ru>
2016-10-22 02:41:20 +0800

    a colspan with a selectRow was fixed

--- src/TableHeader.js
+++ src/TableHeader.js
@@ -31,8 +31,6 @@ class TableHeader extends Component {
       'table-bordered': this.props.bordered,
       'table-condensed': this.props.condensed
     }, this.props.tableHeaderClass);
-    let selectRowHeaderCol = null;
-    if (!this.props.hideSelectColumn) selectRowHeaderCol = this.renderSelectRowHeader();
     let i = 0;
 
     let rowCount = 0;
@@ -41,7 +39,13 @@ class TableHeader extends Component {
         rowCount = Number(elm.props.row);
       }
     });
+
     const rows = [];
+
+    if (!this.props.hideSelectColumn) {
+      rows[0] = [ this.renderSelectRowHeader(rowCount + 1) ];
+    }
+
     React.Children.forEach(this.props.children, (elm) => {
       const { sortIndicator, sortName, sortOrder, onSort } = this.props;
       const { dataField, dataSort } = elm.props;
@@ -74,7 +78,6 @@ class TableHeader extends Component {
       <div ref='container' className={ containerClasses } style={ this.props.style }>
         <table className={ tableClasses }>
           <thead ref='header'>
-            { selectRowHeaderCol }
             { trs }
           </thead>
         </table>
@@ -82,21 +85,21 @@ class TableHeader extends Component {
     );
   }
 
-  renderSelectRowHeader() {
+  renderSelectRowHeader(rowCount) {
     if (this.props.customComponent) {
       const CustomComponent = this.props.customComponent;
       return (
-        <SelectRowHeaderColumn>
+        <SelectRowHeaderColumn rowCount={ rowCount }>
           <CustomComponent type='checkbox' checked={ this.props.isSelectAll }
             indeterminate={ this.props.isSelectAll === 'indeterminate' } disabled={ false }
             onChange={ this.props.onSelectAllRow } rowIndex='Header'/>
         </SelectRowHeaderColumn>
       );
     } else if (this.props.rowSelectType === Const.ROW_SELECT_SINGLE) {
-      return (<SelectRowHeaderColumn />);
+      return (<SelectRowHeaderColumn rowCount={ rowCount }/>);
     } else if (this.props.rowSelectType === Const.ROW_SELECT_MULTI) {
       return (
-        <SelectRowHeaderColumn>
+        <SelectRowHeaderColumn rowCount={ rowCount }>
           <Checkbox
             onChange={ this.props.onSelectAllRow }
             checked={ this.props.isSelectAll }/>
