4ad6076bddfb68f2e712ebf4afd0424b10c2bea8
AllenFang <ayu780129@hotmail.com>
2016-12-18 15:08:39 +0800

    example for #897

--- examples/js/expandRow/demo.js
+++ examples/js/expandRow/demo.js
@@ -1,6 +1,7 @@
 /* eslint max-len: 0 */
 import React from 'react';
 import ExpandRow from './ExpandRow';
+import ExpandByColumn from './expand-row-by-column';
 import renderLinks from '../utils';
 
 import { Col, Panel } from 'react-bootstrap';
@@ -13,6 +14,12 @@ class Demo extends React.Component {
           { renderLinks('expandRow/expandRow.js') }
           <p>You can insert your customize component as a expand component</p>
           <ExpandRow/>
+        </Panel>
+        <Panel header={ 'Expand Row Trigger by Column instead of Row' }>
+          { renderLinks('expandRow/expand-row-by-column.js') }
+          <span>You can trigger expand row by column or row, default is row,
+          configure <code>expandBy</code> in <code>options</code> props</span>
+          <ExpandByColumn/>
         </Panel>
       </Col>
     );
