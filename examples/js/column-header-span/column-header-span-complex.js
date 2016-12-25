167ad61e903b3205d8316f9f70dd9c03ad4c1b24
AllenFang <ayu780129@hotmail.com>
2016-12-25 16:06:09 +0800

    update example

--- examples/js/column-header-span/column-header-span-complex.js
+++ examples/js/column-header-span/column-header-span-complex.js
@@ -13,6 +13,7 @@ function addProducts(quantity) {
       id: id,
       name: 'Item name ' + id,
       price: 2100 + i,
+      coupon: Math.round(Math.random()) ? 'yes' : 'no',
       status: Math.round(Math.random()) ? 'yes' : 'no',
       customer: 'Customer ' + i,
       order: i
@@ -27,7 +28,6 @@ export default class ColumnHeaderSpanComplex extends React.Component {
   render() {
     const selectRow = {
       mode: 'checkbox',
-      clickToSelect: true,
       bgColor: 'rgb(238, 193, 213)'
     };
 
@@ -41,13 +41,14 @@ export default class ColumnHeaderSpanComplex extends React.Component {
         cellEdit={ cellEdit }
         insertRow deleteRow exportCSV>
         <TableHeaderColumn row='0' rowSpan='2' dataField='id' isKey={ true } >ID</TableHeaderColumn>
-        <TableHeaderColumn row='0' colSpan='2' csvHeader='Product'>Product</TableHeaderColumn>
-        <TableHeaderColumn row='1' dataField='name'>name</TableHeaderColumn>
-        <TableHeaderColumn row='1' dataField='price'>price</TableHeaderColumn>
+        <TableHeaderColumn row='0' colSpan='3' dataSort csvHeader='Product' headerAlign='right'>Product</TableHeaderColumn>
+        <TableHeaderColumn row='1' dataField='name' width='175' dataAlign='center'>name</TableHeaderColumn>
+        <TableHeaderColumn row='1' dataField='price' dataSort>price</TableHeaderColumn>
+        <TableHeaderColumn row='1' dataField='coupon' width='70'>Coupon</TableHeaderColumn>
         <TableHeaderColumn row='0' csvHeader='In stock' rowSpan='2' dataField='status'>In stock</TableHeaderColumn>
-        <TableHeaderColumn row='0' colSpan='2' csvHeader='Customer'>Customer</TableHeaderColumn>
+        <TableHeaderColumn row='0' colSpan='2' csvHeader='Customer' filter={ { type: 'TextFilter', delay: 1000 } }>Customer</TableHeaderColumn>
         <TableHeaderColumn row='1' csvHeader='name' dataField='customer'>name</TableHeaderColumn>
-        <TableHeaderColumn row='1' csvHeader='order' dataField='order'>order</TableHeaderColumn>
+        <TableHeaderColumn row='1' csvHeader='order' dataField='order' dataSort>order</TableHeaderColumn>
       </BootstrapTable>
     );
   }
