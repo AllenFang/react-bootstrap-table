e0b6e47622bb11b5e41bdc707eb2c706a59fc6a6
AllenFang <ayu780129@hotmail.com>
2017-02-12 17:08:20 +0800

    add examples for keyboard navigation

---
+++ examples/js/keyboard-nav/enter-to-edit-with-nav-table.js
@@ -0,0 +1,39 @@
+/* eslint max-len: 0 */
+import React from 'react';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+
+
+const products = [];
+
+function addProducts(quantity) {
+  const startId = products.length;
+  for (let i = 0; i < quantity; i++) {
+    const id = startId + i;
+    products.push({
+      id: id,
+      name: 'Item name ' + id,
+      price: 2100 + i
+    });
+  }
+}
+
+addProducts(5);
+
+export default class EnterToEditWithNavTable extends React.Component {
+  render() {
+    const cellEdit = {
+      mode: 'click',
+      blurToSave: true
+    };
+    const keyBoardNav = {
+      enterToEdit: true
+    };
+    return (
+      <BootstrapTable data={ products } cellEdit={ cellEdit } keyBoardNav={ keyBoardNav }>
+          <TableHeaderColumn dataField='id' isKey={ true }>Product ID</TableHeaderColumn>
+          <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
+          <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
+      </BootstrapTable>
+    );
+  }
+}
---
+++ examples/js/keyboard-nav/nav-with-cell-edit-table.js
@@ -0,0 +1,36 @@
+/* eslint max-len: 0 */
+import React from 'react';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+
+
+const products = [];
+
+function addProducts(quantity) {
+  const startId = products.length;
+  for (let i = 0; i < quantity; i++) {
+    const id = startId + i;
+    products.push({
+      id: id,
+      name: 'Item name ' + id,
+      price: 2100 + i
+    });
+  }
+}
+
+addProducts(5);
+
+export default class CellEditWithNavTable extends React.Component {
+  render() {
+    const cellEdit = {
+      mode: 'click',
+      blurToSave: true
+    };
+    return (
+      <BootstrapTable data={ products } cellEdit={ cellEdit } keyBoardNav>
+          <TableHeaderColumn dataField='id' isKey={ true }>Product ID</TableHeaderColumn>
+          <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
+          <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
+      </BootstrapTable>
+    );
+  }
+}
