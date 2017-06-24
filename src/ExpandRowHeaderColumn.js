2cf11ec951089ff6452c0a97ee0613fc113103c5
AllenFang <ayu780129@hotmail.com>
2017-06-24 15:46:55 +0800

    fix #1417

---
+++ src/ExpandRowHeaderColumn.js
@@ -0,0 +1,19 @@
+import React, { Component, PropTypes } from 'react';
+
+class ExpandRowHeaderColumn extends Component {
+
+  render() {
+    return (
+      <th rowSpan={ this.props.rowCount } style={ { textAlign: 'center' } }
+        className='react-bs-table-expand-cell'
+        data-is-only-head={ false }>
+        { this.props.children }
+      </th>
+    );
+  }
+}
+ExpandRowHeaderColumn.propTypes = {
+  children: PropTypes.node,
+  rowCount: PropTypes.number
+};
+export default ExpandRowHeaderColumn;
