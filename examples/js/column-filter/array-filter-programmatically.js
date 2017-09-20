a4f5b55d5953f6485ceb3435dd4012439ea33ff5
AllenFang <ayu780129@hotmail.com>
2017-09-20 20:22:24 +0800

    add array filter example

---
+++ examples/js/column-filter/array-filter-programmatically.js
@@ -0,0 +1,42 @@
+/* eslint max-len: 0 */
+import React from 'react';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+
+const products = [];
+
+function addProducts(quantity) {
+  const startId = products.length;
+  for (let i = 0; i < quantity; i++) {
+    const id = startId + i;
+    products.push({
+      id: id,
+      name: 'Item name ' + id,
+      price: 2100 + i
+    });
+  }
+}
+
+addProducts(5);
+
+export default class ProgrammaticallyArrayFilter extends React.Component {
+  /* Filtering passing an array of values */
+  handleBtnClick = () => {
+    this.refs.table.handleFilterData({
+      name: { type: 'ArrayFilter', value: [ 'Item name 3', 'Item name 4' ] },
+      price: { type: 'ArrayFilter', value: [ 2100, 2104 ] }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={ this.handleBtnClick } className='btn btn-default'>Click to apply text filter</button>
+        <BootstrapTable ref='table' data={ products }>
+          <TableHeaderColumn dataField='id' isKey={ true }>Product ID</TableHeaderColumn>
+          <TableHeaderColumn dataField='name' filter={ { type: 'TextFilter', delay: 1000 } }>Product Name</TableHeaderColumn>
+          <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
+        </BootstrapTable>
+      </div>
+    );
+  }
+}
